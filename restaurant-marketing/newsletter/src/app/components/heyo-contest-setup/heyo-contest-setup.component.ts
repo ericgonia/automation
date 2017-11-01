77204740bc170533acdaedee7ddb01f967e63730
Eric Gonia <eric.gonia@gmail.com>
2017-10-31 20:38:30 -0500

    Add contest end date.

--- restaurant-marketing/newsletter/src/app/components/heyo-contest-setup/heyo-contest-setup.component.ts
+++ restaurant-marketing/newsletter/src/app/components/heyo-contest-setup/heyo-contest-setup.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeyoContestSetupComponent implements OnInit {
 
+  contestEndDate: string;
+  
   constructor() { }
 
   ngOnInit() {
