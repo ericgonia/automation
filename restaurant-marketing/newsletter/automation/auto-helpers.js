2071bda891c747505daf9bf3e64ba8cacfe5fd0a
Eric Gonia <eric.gonia@gmail.com>
2017-11-07 20:19:33 -0600

    Refactoring.

---
+++ restaurant-marketing/newsletter/automation/auto-helpers.js
@@ -0,0 +1,20 @@
+const { By, Key, until } = require('selenium-webdriver');
+
+function AutoHelpers(driver) {
+    this.driver = driver;
+    this.clickElement = function (xpath) {
+        var me = this;
+        return new Promise((resolve, reject) => {
+            me.driver.wait(until.elementLocated(By.xpath(xpath)), 5000).then(el => {
+                me.driver.wait(until.elementIsVisible(el), 5000).then(vel => {
+                    return vel.click().then(() => resolve(), (err) => reject(err));
+                })
+            }).catch((error) => reject(error));
+        });
+    }
+}
+
+
+
+
+module.exports = AutoHelpers;
--- restaurant-marketing/newsletter/automation/heyo-automation.js
+++ restaurant-marketing/newsletter/automation/heyo-automation.js
@@ -1,20 +1,27 @@
 const { Builder, By, Key, until } = require('selenium-webdriver');
 const { config } = require('../config');
+var AutoHelpers = require('./auto-helpers');
 
 let driver = null;
+let autoHelpers = null;
 
 function setupContest(type) {
     const userId = config.heyo.userId;
     const password = config.heyo.password;
 
     driver = new Builder()
-    .forBrowser('chrome')
-    .build();
+        .forBrowser('chrome')
+        .build();
+    autoHelpers = new AutoHelpers(driver);
+
     driver.get('https://www.heyo.com/app/login');
 
     loginToHeyo(userId, password).then(() => {
-        console.log("login successful");
-    });
+        duplicateCampaign().then(() => {
+            console.log('campaign duplicated');
+        }, (error) => console.log(error));
+    }, (error) => console.log(error));
+
     //driver.wait(until.titleIs('webdriver - Google Search'), 1000);
     //driver.quit();
 }
@@ -25,7 +32,18 @@ function loginToHeyo(userId, password) {
         driver.findElement(By.name("username")).sendKeys(userId);
         driver.findElement(By.name("password")).sendKeys(password);
         driver.findElement(By.xpath("//button[@type='submit']")).click();
-        driver.wait(until.titleIs("Heyo :: Bjs Market Cafe"), 10000, "Waited for login to be successful, but never found the Search Campaigns input.").then(()=>resolve());
+        driver.wait(until.titleIs("Heyo :: Bjs Market Cafe"), 10000, "Waited for login to be successful, but the title never matched.").then(() => resolve(), (error) => reject(error));
+    });
+}
+
+
+function duplicateCampaign() {
+    return new Promise((resolve, reject) => {
+        autoHelpers.clickElement("//*[@id=\"drop_heyo225_views_form_Dropdown_1\"]").then(() => {
+            autoHelpers.clickElement("//*[@id=\"heyo225_views_form_Dropdown_1\"]/ul/li[1]/a").then(() => {
+                resolve();
+            }, (error) => reject(error));
+        }, (error) => reject(error));
     });
 }
 
